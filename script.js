c5a525b86d28108a53bf6e8c7ffb75144e13ac99
Honor-MB <adeabdulmuizz@gmail.com>
2026-04-02 12:34:32 +0100

    Responsiveness

--- script.js
+++ script.js
@@ -6,17 +6,26 @@ const addition = document.querySelector("#add")
 const subtraction = document.querySelector("#subtract")
 const displayAnswer = document.querySelector("#show")
 
+const error = () =>
+{
+    if(!myFirstNum.value || !mySecondNum.value)
+    {
+        return displayAnswer.textContent = "Syntax Error" 
+    }
+}
 
 multiplication.addEventListener("click", () => 
 {
     let answer = myFirstNum.value * mySecondNum.value
     displayAnswer.textContent = answer
+    error()
 })
 
 division.addEventListener("click", () => 
 {
     let answer = (myFirstNum.value / mySecondNum.value).toFixed(8)
     displayAnswer.textContent = answer
+    error()
 })
 
 addition.addEventListener("click", () => 
@@ -25,10 +34,12 @@ addition.addEventListener("click", () =>
     const mySecondNum = parseInt(document.querySelector("#secondnum").value)
     let answer = (myFirstNum + mySecondNum)
     document.querySelector("#show").textContent = answer
+    error()
 })
 
 subtraction.addEventListener("click", () => 
 {
     let answer = myFirstNum.value - mySecondNum.value
     displayAnswer.textContent = answer
+    error()
 })
